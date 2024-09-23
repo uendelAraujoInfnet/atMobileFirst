e93bb31dad782490d516465d617b8026949ff7b1
uendelAraujoInfnet <uendel.araujo@al.infnet.edu.br>
2024-09-23 10:20:36 -0400

    Exercicio 03 - Layout Com Flex, Justify e Align

---
+++ 03-layout-flex-justify-align/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LayoutGaleria from './components/LayoutGaleria'
+
+
+
+const App = () => {
+  return (
+    <div >
+      <LayoutGaleria/>
+    </div>
+  )
+}
+
+export default App
+
---
+++ 03-layout-flex-justify-align/src/components/LayoutGaleria.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import styles from "./LayoutGaleria.module.css";
+
+const LayoutGaleria = () => {
+  const [images, setImages] = useState(5);
+
+  const addImage = () => setImages(images + 1);
+  const removeImage = () => setImages(images > 0 ? images - 1 : 0);
+
+
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.controls}>
+        <button onClick={addImage}>+</button>
+        <span>{images}</span>
+        <button onClick={removeImage}>-</button>
+      </div>
+
+      <div className={styles.gallery}>
+        {Array.from({length: images}).map((_, index) => (
+          <div key={index} className={styles.imagePlaceholder}>
+            <p>100 x 100</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LayoutGaleria
