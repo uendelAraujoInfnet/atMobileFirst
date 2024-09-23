f0a6162350b9e0fd80684b7c041187a3e8b81329
uendelAraujoInfnet <uendel.araujo@al.infnet.edu.br>
2024-09-23 13:01:10 -0400

    Exercicio 04 - Layout com CSS Grid

---
+++ 04-layout-css-grid/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LayoutGrid from './components/LayoutGrid'
+
+const App = () => {
+  return (
+    <div >
+      <LayoutGrid />
+    </div>
+  )
+}
+
+export default App
+
