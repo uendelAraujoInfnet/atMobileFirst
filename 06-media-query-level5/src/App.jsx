a5a084f7db23048f8a27896cd747fab6af5f4ffe
uendelAraujoInfnet <uendel.araujo@al.infnet.edu.br>
2024-09-24 13:29:55 -0400

    Exercicio 06 - Media Query Level 5

---
+++ 06-media-query-level5/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ItemsList from "./components/ItemsList";
+
+const App = () => {
+  return (
+    <div>
+      <ItemsList />
+    </div>
+  );
+};
+
+export default App;
