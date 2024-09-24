11e90f95e9b414ac7c00bfca65c10ef6a9998392
uendelAraujoInfnet <uendel.araujo@al.infnet.edu.br>
2024-09-24 11:32:26 -0400

    Exercicio 05 - Layout Responsivo

---
+++ 05-layout-responsivo/src/components/LayoutResponsivo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from "./LayoutResponsivo.module.css"
+
+const LayoutResponsivo = () => {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>Header</header>
+      <nav className={styles.globalMenu}>Global Menu</nav>
+      <aside className={styles.contextMenu}>Context Menu</aside>
+      <main className={styles.mainContent}>Main Content</main>
+      <aside className={styles.ads}>Ads</aside>
+      <footer className={styles.footer}>Footer</footer>
+    </div>
+  )
+}
+
+export default LayoutResponsivo
